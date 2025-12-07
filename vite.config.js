346ff097567cc6de3c231656c1dae4eba30eaa7c
Pierce Issah <PJIceskull2003@gmail.com>
2025-12-07 14:46:17 -0500

    remove plugin from config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
-import vitePluginSftp from "vite-plugin-sftp";
 
 export default defineConfig({
   // Vite Configuration options can be added here
   base: "./",
-  plugins: [vitePluginSftp()],
+  plugins: [],
   server: {
     cors: {
       origin: ["http://localhost:3000", "https://comicvine.gamespot.com/api/"],
