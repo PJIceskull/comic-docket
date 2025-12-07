797ff9146fa083974d95b114100a8d17ecabcf8d
Pierce Issah <PJIceskull2003@gmail.com>
2025-12-07 13:07:13 -0500

    install vite plugin for SFTP

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
+import vitePluginSftp from "vite-plugin-sftp";
 
 export default defineConfig({
   // Vite Configuration options can be added here
   base: "./",
+  plugins: [vitePluginSftp()],
+  server: {
+    cors: {
+      origin: ["http://localhost:3000", "https://comicvine.gamespot.com/api/"],
+      methods: ["GET", "POST"], // Allowed HTTP methods
+      credentials: true,
+    },
+  },
 });
